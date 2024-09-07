905f9b1157ffc601d55403f9594e6f88ae0fc034
يوسف جمال يوسف محمد ناجي <hiet122230340@Sinaieng.edu.eg>
2024-09-07 21:09:54 +0300

    Update get_attribute_value.py

--- get_attribute_value.py
+++ get_attribute_value.py
@@ -1,9 +1,12 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.common.exceptions import TimeoutException
 def get_attribute_value(driver, selector, attribute):
     """Get the value of a specified attribute from an element identified by a CSS selector."""
     # Wait for the element to be visible
-    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, selector)))
-    return element.get_attribute(attribute)
+    try:
+        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, selector)))
+        return element.get_attribute(attribute)
+    except TimeoutException:
+        print(f"error with selector {selector}")
