08252d6ea82273e6a0502009ba3c00610be4efd5
يوسف جمال يوسف محمد ناجي <hiet122230340@Sinaieng.edu.eg>
2024-09-07 21:08:59 +0300

    Update get_inner_text.py

--- get_inner_text.py
+++ get_inner_text.py
@@ -1,9 +1,13 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 def get_inner_text(driver, selector):
     """Get the inner text of an element identified by a CSS selector."""
     # Wait for the element to be visible
-    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, selector)))
-    return element.text
+    try:
+        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, selector)))
+        return element.text
+    except TimeoutException:
+        print(f"Timeout exception with element selector : {selector}")
